2c7f1b8d6f0e6b7303251c1995ce6cc79b9caac0
王伟晶-Weijing <weijing.wang@d1m.cn>
2021-11-03 14:08:28 +0800

    希尔排序修改

--- 算法/排序算法/希尔排序/shell.js
+++ 算法/排序算法/希尔排序/shell.js
@@ -4,14 +4,14 @@ function ShellSort(arr) {
   var len = arr.length
   for (var gap = Math.floor(len / 2); gap > 0; gap = Math.floor(gap / 2)) {
     for (var i = gap; i < len; i++) {
-      var j = i + gap
-      while (j > 0 && j < len) {
-        if (arr[j] < arr[j - gap]) {
-          [arr[j], arr[j - gap]] = [arr[j - gap], arr[j]]
-        } else {
-          break
+      var j = i
+      var tmp = arr[j]
+      if(arr[j] < arr[j - gap]){
+        while (j - gap >=0 && tmp < arr[j - gap]) {
+          arr[j] = arr[j - gap]
+          j -= gap
         }
-        j -= gap
+        arr[j] = tmp
       }
     }
   }
