151b03afe28631e1c39a5ccdf26134902d23e00e
王伟晶-Weijing <weijing.wang@d1m.cn>
2021-11-15 18:17:37 +0800

    桶排序

---
+++ 算法/排序算法/桶排序/bucketSort.js
@@ -0,0 +1,32 @@
+function bucketSort(arr) {
+  // 找到最大值与最小值
+  let max = -Infinity,
+    min = Infinity
+  for (let v of arr) {
+    max = Math.max(max, v)
+    min = Math.min(min, v)
+  }
+  // 生成桶数组
+  const bucketNum = Math.floor((max - min) / arr.length) + 1 //桶数量
+  let bucketArr = new Array(bucketNum).fill(null)
+  bucketArr = bucketArr.map(_ => [])
+  // 每个元素放入桶
+  for (let v of arr) {
+    let index = Math.floor((v - min) / arr.length)
+    bucketArr[index].push(v)
+  }
+  // 结果数组
+  const res = []
+  for (let item of bucketArr) {
+    // 可进行其他排序
+    item.sort((a, b) => a - b)
+    // 输出
+    for (let v of item) {
+      res.push(v)
+    }
+  }
+  return res
+}
+
+const arr = [14, 28, 10, 24, 35, 50, 39, 47]
+console.log(bucketSort(arr))
