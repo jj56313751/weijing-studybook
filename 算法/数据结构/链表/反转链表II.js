6c5115b5db7f4554433610b44ff020ec3ccfee8b
王伟晶-Weijing <weijing.wang@d1m.cn>
2021-11-25 20:39:58 +0800

    反转链表

---
+++ 算法/数据结构/链表/反转链表II.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function (head, left, right) {
+  var ret = new ListNode(null, head)
+  var cnt = right - left + 1
+  var p = ret
+  while (--left) {
+    p = p.next
+  }
+  p.next = reverse(p.next, cnt) //必须反转p.next，如果直接反转p，p的前一个元素仍然指向p，中间反转的部分都被略过了
+  return ret.next
+};
+
+function reverse(head, cnt) {
+  var p = null,
+    q = ret = head
+  while (cnt--) {
+    head = head.next
+    q.next = p
+    p = q
+    q = head
+  }
+  ret.next = q // 原来的头节点已经指向了null，再连接上反转后的下一个元素
+  return p
+}
